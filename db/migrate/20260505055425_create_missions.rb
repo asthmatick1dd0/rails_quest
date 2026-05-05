41117fbaca3a7de7c08e2b6b9796ae2591a1c4e4
Andrew Khrobostov <andrew.khrobostov@gmail.com>
2026-05-05 09:05:54 +0300

    Change models to be not nullable and unique

--- db/migrate/20260505055425_create_missions.rb
+++ db/migrate/20260505055425_create_missions.rb
@@ -1,11 +1,14 @@
 class CreateMissions < ActiveRecord::Migration[8.1]
   def change
     create_table :missions do |t|
-      t.references :agent, null: false, foreign_key: true
-      t.string :title
-      t.string :status
+      t.bigint :agent_id, null: false
+      t.string :title, null: false
+      t.string :status, null: false
 
       t.timestamps
     end
+
+    add_index :missions, :agent_id
+    add_check_constraint :missions, "status IN ('assigned', 'in_progress', 'completed')", name: "missions_status_valid"
   end
 end
--- db/migrate/20260505055854_create_agents.rb
+++ db/migrate/20260505055854_create_agents.rb
@@ -1,11 +1,14 @@
 class CreateAgents < ActiveRecord::Migration[8.1]
   def change
     create_table :agents do |t|
-      t.string :codename
-      t.integer :level
-      t.boolean :active
+      t.string :codename, null: false
+      t.integer :level, null: false
+      t.boolean :active, null: false
 
       t.timestamps
     end
+
+    add_index :agents, :codename, unique: true
+    add_check_constraint :agents, "level BETWEEN 1 AND 10", name: "agents_level_between_1_and_10"
   end
 end
