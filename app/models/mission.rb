ed8a8022ae9bed29bf77bc7452e32b130a1b3993
Andrew Khrobostov <andrew.khrobostov@gmail.com>
2026-05-05 09:06:46 +0300

    Make models depends on each other

--- app/models/mission.rb
+++ app/models/mission.rb
@@ -1,3 +1,12 @@
 class Mission < ApplicationRecord
   belongs_to :agent
+
+  enum :status, {
+    assigned: "assigned",
+    in_progress: "in_progress",
+    completed: "completed"
+  }, validate: true
+
+  validates :title, presence: true
+  validates :status, presence: true
 end
